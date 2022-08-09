7ceacba831ed911434b9da7fd2a5fc1d8bd65c8d
priyankasinhaba <73702702+priyankasinhaba@users.noreply.github.com>
2022-08-09 18:28:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var towerImg, tower;
+var doorImg, door, doorsGroup;
+var climberImg, climber, climbersGroup;
+var ghost, ghostImg;
+var invisibleBlockGroup, invisibleBlock;
+var gameState = "play"
+
+function preload(){
+  towerImg = loadImage("tower.png");
+  doorImg = loadImage("door.png");
+  climberImg = loadImage("climber.png");
+  ghostImg = loadImage("ghost-standing.png");
+  spookySound = loadSound("spooky.wav");
+}
+
+function setup(){
+  createCanvas(600,600);
+ 
+  tower = createSprite(300,300);
+  tower.addImage("tower",towerImg);
+  tower.velocityY = 1;
+  
+ 
+  
+  ghost = createSprite(200,200,50,50);
+  ghost.scale = 0.3;
+  ghost.addImage("ghost", ghostImg);
+}
+
+function draw(){
+  background(0);
+ 
+    
+    if(tower.y > 400){
+      tower.y = 300
+    }
+   
+    
+    drawSprites();
+  }
+  
+
+
